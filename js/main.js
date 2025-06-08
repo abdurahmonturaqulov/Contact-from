f986b5ab37b5fdac21009fe73417809754a89255
Abdurahmon <rahmlow.1217@mail.ru>
2025-06-08 16:37:24 +0500

    3filessadd

---
+++ js/main.js
@@ -0,0 +1,22 @@
+const fName = document.querySelector('form #fName')
+const age = document.querySelector('form #age')
+const isMarried = document.querySelector('form #isMarried')
+
+const fNameText = document.querySelector('.fName')
+const ageText = document.querySelector('.age')
+const isMarriedText = document.querySelector('.isMarried')
+
+const btnSubmit = document.querySelector(".btnSubmit")
+const btnReset = document.querySelector(".btnReset")
+
+const message = document.querySelector(".message")
+
+fName.addEventListener("input", function (e) {
+       fNameText.innerHTML = `🐱‍💻fName: ${e.target.value}`
+})
+age.addEventListener("input", function (e) {
+       ageText.innerHTML = `🐱‍💻age: ${e.target.value}`
+})
+isMarried.addEventListener("input", function (e) {
+       isMarriedText.innerHTML = `🐱‍💻isMarried: ${e.target.value}`
+})
