de1bc7ed6d9e85c0273c6bef396ef6003c61ef8b
Abdurahmon <rahmlow.1217@mail.ru>
2025-06-08 17:20:24 +0500

    2fileadd

--- js/main.js
+++ js/main.js
@@ -6,17 +6,24 @@ const fNameText = document.querySelector('.fName')
 const ageText = document.querySelector('.age')
 const isMarriedText = document.querySelector('.isMarried')
 
-const btnSubmit = document.querySelector(".btnSubmit")
-const btnReset = document.querySelector(".btnReset")
+const btnSubmit = document.querySelector('.btnSubmit')
+const btnReset = document.querySelector('.btnReset')
 
-const message = document.querySelector(".message")
+const message = document.querySelector('.message')
 
-fName.addEventListener("input", function (e) {
-       fNameText.innerHTML = `🐱‍💻fName: ${e.target.value}`
+fName.addEventListener('input', function (e) {
+  fNameText.innerHTML = `🐱‍💻fName: ${e.target.value}`
 })
-age.addEventListener("input", function (e) {
-       ageText.innerHTML = `🐱‍💻age: ${e.target.value}`
+age.addEventListener('input', function (e) {
+  ageText.innerHTML = `🐱‍💻age: ${e.target.value}`
 })
-isMarried.addEventListener("input", function (e) {
-       isMarriedText.innerHTML = `🐱‍💻isMarried: ${e.target.value}`
+isMarried.addEventListener('input', function (e) {
+  isMarriedText.innerHTML = `🐱‍💻isMarried: ${e.target.value}`
+})
+
+btnSubmit.addEventListener("click", function () {
+       message.innerHTML = "Successful send! ✔"
+})
+btnReset.addEventListener("click", function () {
+       message.innerHTML = "Unsuccessful, try again 🤔"
 })
